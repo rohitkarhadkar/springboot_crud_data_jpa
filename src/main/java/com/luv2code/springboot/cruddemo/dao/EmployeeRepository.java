35bf2669adc2f5e47db4670e0b335cd0d01eeb63
unknown <rohitkarhadkar.rk@gmail.com>
2024-01-31 12:51:25 -0700

    initial commit

---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	//That's it.....no coding required
+}
